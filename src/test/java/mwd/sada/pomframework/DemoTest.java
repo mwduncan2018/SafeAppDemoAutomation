d969eb0a4b56449dedebf1aa5cfcedc0ea94cecd
Michael Duncan <mwduncan2018@gmail.com>
2021-05-09 20:49:26 -0400

    updates

--- src/test/java/mwd/sada/pomframework/DemoTest.java
+++ src/test/java/mwd/sada/pomframework/DemoTest.java
@@ -1,14 +1,23 @@
 package mwd.sada.pomframework;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 class DemoTest {
+	private static Dimension dimension = new Dimension(1200, 1000);
 
 	@Test
-	void test() {
-
+	void seleniumTest1() {
+		//System.setProperty("webdriver.gecko.driver", GECKODRIVER_PATH);
+		WebDriver x = new FirefoxDriver();
+		x.manage().timeouts().implicitlyWait(15000, TimeUnit.MILLISECONDS);
+		x.manage().window().setSize(dimension);
+		x.navigate().to("https://www.pnc.com/en/personal-banking.html");
+		x.close();
 	}
-
 }
